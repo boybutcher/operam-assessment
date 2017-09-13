32351115ec1368860134a43320be0c151963d9eb
boybutcher <BYRONdeLARGE@yahoo.com>
2017-09-13 15:58:04 -0700

    add proxy, remove boilerplate css, add conditional rendering for TreeContainer

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import TreeContainer from './TreeContainer';
 import ControlPanel from './ControlPanel';
 
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tree: [],
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <TreeContainer />
+        <TreeContainer tree={this.state.tree}/>
         <ControlPanel />
       </div>
     );
--- client/src/TreeContainer.js
+++ client/src/TreeContainer.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 
 class TreeContainer extends Component {
   render() {
-    return (
-      <div>Tree Container</div>
-    )
+    
+    var {
+      tree
+    } = this.props;
+
+    if (tree.length === 0) {
+      return (
+        <div>tree is empty!</div>
+      )
+    } else {
+      return (
+        <div>filler tree</div>
+      )
+    }
   }
 }
 
