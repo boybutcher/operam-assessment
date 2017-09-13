a42d03000a421560c33cbe138985e3e4ba85d420
boybutcher <BYRONdeLARGE@yahoo.com>
2017-09-13 15:35:42 -0700

    general front-end boiler plate. add ControlPanel.js & TreeContainer.js

---
+++ client/src/ControlPanel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ControlPanel extends Component {
+  render() {
+    return (
+      <div>Control Panel</div>
+    )
+  }
+}
+
+export default ControlPanel;
---
+++ client/src/TreeContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TreeContainer extends Component {
+  render() {
+    return (
+      <div>Tree Container</div>
+    )
+  }
+}
+
+export default TreeContainer;
