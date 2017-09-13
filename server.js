5c9ae9568adb6bbdb32420084202395ba12ab3ce
boybutcher <BYRONdeLARGE@yahoo.com>
2017-09-13 15:06:59 -0700

    change server to port 3001

--- server.js
+++ server.js
@@ -74,6 +74,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('operam-assessment is listening to port 3000!');
+app.listen(3001, () => {
+  console.log('operam-assessment is listening to port 3001!');
 })
